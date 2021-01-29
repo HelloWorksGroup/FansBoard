6ef07db864fcf31c4d7a9605d681fb93dc032686
xianii <jiyucheng007@gmail.com>
2021-01-29 14:50:05 +0800

    更完善的报错信息

--- apps/fans-board/application.lua
+++ apps/fans-board/application.lua
@@ -17,13 +17,17 @@ function(t)
     print("update")
     http.get(api..uid, nil, 
     function(code, data)
-        if (code < 0) then
-            print("HTTP request failed")
+		print(code, data)
+        if (code ~= 200) then
+			display:update("HTTP RET", -1)
+			display:update(tostring(code), -1)
         else
-            print(code, data)
             local parsed = sjson.decode(data)
-            if (parsed.data~=sjson.NULL) then
-                if (parsed.data.follower ~= sjson.NULL) then
+			if (parsed.code ~= sjson.NULL) then
+				if (parsed.code ~= 0) then
+					display:update("API RET", -1)
+					display:update(tostring(parsed.code), -1)
+				elseif (parsed.data~=sjson.NULL and parsed.data.follower ~= sjson.NULL) then
 					print("Fans:" .. parsed.data.follower)
 					if fans < parsed.data.follower then
 						display:update(tostring(parsed.data.follower), -1)
@@ -31,7 +35,9 @@ function(t)
 						display:update(tostring(parsed.data.follower), 1)
 					end
 					fans = parsed.data.follower
-                end
+				else
+					display:update("ERROR X", -1)
+				end
             end
         end
     end)
